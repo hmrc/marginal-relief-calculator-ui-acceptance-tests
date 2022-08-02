44a91f01d2ca0ccc1a4efd0c0b73bc63b9461305
sara-raj <100203074+Sara-raj@users.noreply.github.com>
2022-08-02 14:10:32 +0100

    runner classes change

--- run_marginal_relief_calculator_a11y_headless_tests.sh
+++ run_marginal_relief_calculator_a11y_headless_tests.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
-ENV=${1:-local}
-BROWSER=${2:-headless-chrome}
-DRIVER=
+DEFAULT_BROWSER=remote-chrome
+BROWSER_TYPE=$1
+ENV=$2
 
-if [ "$BROWSER" = "chrome" ]; then
-    DRIVER="-Dwebdriver.chrome.driver=/usr/local/bin/chromedriver"
-elif [ "$BROWSER" = "firefox" ]; then
-    DRIVER="-Dwebdriver.gecko.driver=/usr/local/bin/geckodriver"
+if [ -z "$BROWSER_TYPE" ]; then
+    echo "BROWSER_TYPE value not set, defaulting to $DEFAULT_BROWSER..."
+    echo ""
 fi
 
 sbt -jvm-debug 5005 -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER "testOnly uk.gov.hmrc.test.ui.cucumber.runner.A11yRunner"
