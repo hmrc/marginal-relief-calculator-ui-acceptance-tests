f30c204bf1b3890119ca1188c9eac2c3a6cf88eb
sivathota226 <108057130+sivathota226@users.noreply.github.com>
2022-09-20 11:08:25 +0100

    fixing formating issues

--- run_marginal_relief_calculator_ui_zap_tests.sh
+++ run_marginal_relief_calculator_ui_zap_tests.sh
@@ -5,8 +5,8 @@ BROWSER_TYPE=$1
 ENV=$2
 
 if [ -z "$BROWSER_TYPE" ]; then
-    echo "BROWSER_TYPE value not set, defaulting to $DEFAULT_BROWSER..."
-    echo ""
+  echo "BROWSER_TYPE value not set, defaulting to $DEFAULT_BROWSER..."
+  echo ""
 fi
 
 # Scalafmt checks have been separated from the test command to avoid OutOfMemoryError in Jenkins
