97ba6f8720c7947fe1add540a47732882458dc60
sivathota226 <108057130+sivathota226@users.noreply.github.com>
2022-11-03 10:33:33 +0000

    MRC-264 - prod verification testing

--- src/test/scala/uk/gov/hmrc/test/ui/pages/mrc/BeforeYouStartPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/mrc/BeforeYouStartPage.scala
@@ -24,7 +24,8 @@ import uk.gov.hmrc.test.ui.pages.BasePage
 object BeforeYouStartPage extends BasePage {
   val beforeYouStartPage    =
     "Calculate Marginal Relief for Corporation Tax - Calculate Marginal Relief for Corporation Tax - GOV.UK"
-  val url: String           = TestConfiguration.url("marginal-relief-calculator-frontend")
+  //val url: String           = TestConfiguration.url("marginal-relief-calculator-frontend")
+  val url:String="https://www.development.tax.service.gov.uk/marginal-relief-calculator"
   val mrcBeforeYouStartPage =
     "Calculate Marginal Relief for Corporation Tax - Calculate Marginal Relief for Corporation Tax - GOV.UK"
 
