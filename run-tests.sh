3104bdc7530d145ca9500711cf8091cdeac0597f
Jonathan Rea <65158821+jonathanrea@users.noreply.github.com>
2024-03-12 11:39:45 +0000

    Added ZAP testing and updated README

--- run-tests.sh
+++ run-tests.sh
@@ -2,5 +2,36 @@
 
 BROWSER=$1
 ENVIRONMENT=$2
+DAST=${3:-false}
 
-sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
+
+if ($DAST); then
+  export ZAP_FORWARD_ENABLE="true"
+  export ZAP_FORWARD_PORTS=$(lsof -i -P | grep LISTEN | grep :$PORT | grep java | awk '{ print $9}' | sed 's/\*://g' | paste -sd " " -)
+
+  export ZAP_LOCAL_ALERT_FILTERS="${PWD}/alert-filters.json"
+
+  (
+    cd $WORKSPACE/dast-config-manager
+    make local-zap-running
+  )
+
+  echo "started dast-config-manager"
+  echo "=========================================="
+  echo "Browser:              ${BROWSER}"
+  echo "Env:                  ${ENV}"
+  echo "ZAP Proxy Required:   true"
+  echo "ZAP alert filters:    ${ZAP_LOCAL_ALERT_FILTERS}"
+  echo "=========================================="
+fi
+
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dsecurity.assessment="${DAST}" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
+
+if ($DAST); then
+  echo "stopping dast-config-manager"
+  (
+    cd $WORKSPACE/dast-config-manager
+    make local-zap-stop
+  )
+  cd -
+fi
