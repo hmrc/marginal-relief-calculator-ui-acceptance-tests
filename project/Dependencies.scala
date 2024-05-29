93bfb9602079c86251607f52e4d92fab9e16ac42
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-05-29 15:00:51 +0100

    DLS-10273: Updates to plugins and scala version, and rearanges dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,14 +4,13 @@ object Dependencies {
 
   val test = Seq(
     "uk.gov.hmrc"         %% "ui-test-runner"  % "0.19.0"   % Test,
-    "org.scalatest"       %% "scalatest"       % "3.2.18"   % Test,
     "org.scalatestplus"   %% "selenium-4-2"    % "3.2.13.0" % Test,
-    "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8"   % Test,
     "io.cucumber"         %% "cucumber-scala"  % "8.20.0"   % Test,
     "io.cucumber"          % "cucumber-junit"  % "7.15.0"   % Test,
+    "org.scalatest"       %% "scalatest"       % "3.2.18"   % Test,
     "junit"                % "junit"           % "4.13.2"   % Test,
     "com.novocode"         % "junit-interface" % "0.11"     % Test,
-    "com.typesafe"         % "config"          % "1.4.2"    % Test
+    "com.typesafe"         % "config"          % "1.4.3"    % Test
   )
 
 }
