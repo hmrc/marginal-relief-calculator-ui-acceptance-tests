8c2673a05c8f42579d338d102279a6baea52439b
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-05-29 15:14:23 +0100

    DLS-10273: Dependencies updated

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,11 +5,11 @@ object Dependencies {
   val test = Seq(
     "uk.gov.hmrc"         %% "ui-test-runner"  % "0.19.0"   % Test,
     "org.scalatestplus"   %% "selenium-4-2"    % "3.2.13.0" % Test,
-    "io.cucumber"         %% "cucumber-scala"  % "8.20.0"   % Test,
-    "io.cucumber"          % "cucumber-junit"  % "7.15.0"   % Test,
+    "io.cucumber"         %% "cucumber-scala"  % "8.23.0"   % Test,
+    "io.cucumber"          % "cucumber-junit"  % "7.18.0"   % Test,
     "org.scalatest"       %% "scalatest"       % "3.2.18"   % Test,
     "junit"                % "junit"           % "4.13.2"   % Test,
-    "com.novocode"         % "junit-interface" % "0.11"     % Test,
+    "com.github.sbt"       % "junit-interface" % "0.13.3"   % Test,
     "com.typesafe"         % "config"          % "1.4.3"    % Test
   )
 
